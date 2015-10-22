6300cdf7b9e011c139de1717bd0a8f818634c57f
Scott Opell <me@scottopell.com>
2015-10-22 01:33:48 -0400

    assist wrapper supports either line and column or file offset

--- complete.rb
+++ complete.rb
@@ -4,17 +4,23 @@ require 'stringio'
 
 file_contents = IO.read ARGF.argv[0]
 
-target_line = ARGF.argv[1].to_i
-target_offset = ARGF.argv[2].to_i
-
 file_offset = 0
 
-File.open(ARGF.argv[0]).each.with_index do |line, num|
-  if (num + 1) == target_line
-    file_offset += target_offset
-    break
+if ARGF.argv.length == 3
+  target_line = ARGF.argv[1].to_i
+  target_offset = ARGF.argv[2].to_i
+
+  File.open(ARGF.argv[0]).each.with_index do |line, num|
+    if (num + 1) == target_line
+      file_offset += target_offset
+      break
+    end
+    file_offset += line.length
   end
-  file_offset += line.length
+elsif ARGF.argv.length == 2
+  file_offset = ARGF.argv[1].to_i
+else
+  puts "Usage: ./complete.rb <filepath> (<char offset>)|(<line> <column>)"
 end
 
 json_output = `curl -s -G "http://localhost:8080/xtext-service/assist?resource=text.statemachine&caretOffset=#{file_offset}" --data-urlencode "fullText=#{file_contents}" -X POST`
